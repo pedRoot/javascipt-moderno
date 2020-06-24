ca117c008812cc65242c54d6ebe6feae14ef62b3
= <you@example.com>
2020-06-24 19:59:43 -0400

    Practica de modules es6 y webpack

---
+++ modules/src/index.js
@@ -0,0 +1,5 @@
+import capitalizar from "./strings.js";
+import _ from "lodash";
+
+console.log(capitalizar("Hola mundo..."));
+console.log(_.capitalize("Hola mundo con lodash..."));
---
+++ modules/src/strings.js
@@ -0,0 +1,5 @@
+function capitalizar(text) {
+  return text.substring(0, 1).toUpperCase() + text.substring(1, text.length);
+}
+
+export default capitalizar;
